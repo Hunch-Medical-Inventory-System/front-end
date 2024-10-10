98343d877ddd83abe2eeec73db6dd8af45fa2103
SuperJu5tin <superjuslib@gmail.com>
2024-10-10 10:17:10 -0600

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/front_end/",
+  base: "/front-end/",
   plugins: [
     VueRouter(),
     Layouts(),
