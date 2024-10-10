c485c70751161bdc40716fbf7b68a850e5e7e3af
SuperJu5tin <superjuslib@gmail.com>
2024-10-10 10:49:48 -0600

    simple fixes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,25 +8,29 @@
   <v-container class="my-15">
     <v-row>
       <v-container class="justify-center">
-        <v-row class="text-h2 justify-center">Medical Based</v-row>
-        <v-row class="text-h1 justify-center my-5">Inventory System</v-row>
-        <v-row class="ga-15 mx-15 my-10">
+        <v-row class="text-h2 justify-center text-center">Medical Based</v-row>
+        <v-row class="text-h1 justify-center text-center my-5">Inventory System</v-row>
+        <v-row class="">
           <v-col>
-            <v-img :src="nasaHunch"/>
+            <v-img max-height="250px" :src="nasaHunch"/>
           </v-col>
           <v-col>
-            <v-img :src="logo" />
+            <v-img max-height="250px" :src="logo" />
           </v-col>
           <v-col>
-            <v-img :src="warrenTech"/>
+            <v-img max-height="250px" :src="warrenTech"/>
           </v-col>
         </v-row>
       </v-container>
     </v-row>
     <!-- <v-col cols="12" sm="8" md="6"> -->
-    <v-row class="text-center justify-center ga-15">
-      <v-btn class="button-styles elevation-10" size="x-large" to="/login">Demo Login</v-btn>
-      <v-btn class="button-styles" size="x-large" to="/profile">Demo Profile</v-btn>
+    <v-row class="text-center justify-center">
+      <v-col>
+        <v-btn class="button-styles elevation-10" size="x-large" to="/login">Demo Login</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn class="button-styles" size="x-large" to="/profile">Demo Profile</v-btn>
+      </v-col>
     </v-row>
     <!-- </v-col> -->
   </v-container>
